e8c8c5735e6c6e18b8387516b51c5c4f38aab554
Mathias Buus <mathiasbuus@gmail.com>
2015-01-09 00:31:59 +0100

    add example

---
+++ example.js
@@ -0,0 +1,17 @@
+var mdns = require('./')()
+
+mdns.on('response', function(response) {
+  console.log('got a response packet:', response)
+})
+
+mdns.on('query', function(query) {
+  console.log('got a query packet:', query)
+})
+
+// lets query for an A record for 'brunhilde.local'
+mdns.query({
+  questions:[{
+    name: 'brunhilde.local',
+    type: 'A'
+  }]
+})
