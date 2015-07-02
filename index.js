684071e262e7d4941312ae1143a0c5689965fc8e
Mathias Buus <mathiasbuus@gmail.com>
2015-07-02 23:11:34 +0200

    cleanup #11

--- index.js
+++ index.js
@@ -12,75 +12,71 @@ module.exports = function (opts) {
   var port = opts.port || 5353
   var type = opts.type || 'udp4'
   var ip = opts.ip || opts.host || (type === 'udp4' ? '224.0.0.251' : null)
-  var sockets = []
-  var clientSocket
 
   if (type === 'udp6' && (!ip || !opts.interface)) {
     throw new Error('For IPv6 multicast you must specify `ip` and `interface`')
   }
 
-  function binder (port) {
-    var bind = thunky(function (cb) {
-      var socket = dgram.createSocket({
-        type: type,
-        reuseAddr: opts.reuseAddr !== false,
-        toString: function () {
-          return type
-        }
-      })
-
-      socket.on('error', cb)
-      socket.on('message', function (message, rinfo) {
-        try {
-          message = packets.decode(message)
-        } catch (err) {
-          that.emit('warning', err)
-          return
-        }
-
-        that.emit('packet', message, rinfo)
-
-        if (message.type === 'query') that.emit('query', message, rinfo)
-        if (message.type === 'response') that.emit('response', message, rinfo)
-      })
-
-      socket.bind(port, function () {
-        if (opts.multicast !== false) {
-          socket.addMembership(ip, opts.interface)
-          socket.setMulticastTTL(opts.ttl || 255)
-          socket.setMulticastLoopback(opts.loopback !== false)
-        }
-
-        socket.removeListener('error', cb)
-        socket.on('error', function (err) {
-          that.emit('warning', err)
-        })
-
-        that.emit('ready')
-        sockets.push(socket)
-        cb(null, socket)
-      })
+  var createSocket = function () {
+    var socket = dgram.createSocket({
+      type: type,
+      reuseAddr: opts.reuseAddr !== false,
+      toString: function () {
+        return type
+      }
     })
 
-    return bind
+    socket.on('error', function (err) {
+      that.emit('warning', err)
+    })
+
+    socket.on('message', function (message, rinfo) {
+      try {
+        message = packets.decode(message)
+      } catch (err) {
+        that.emit('warning', err)
+        return
+      }
+
+      that.emit('packet', message, rinfo)
+
+      if (message.type === 'query') that.emit('query', message, rinfo)
+      if (message.type === 'response') that.emit('response', message, rinfo)
+    })
+
+    socket.on('listening', function () {
+      if (opts.multicast !== false) {
+        socket.addMembership(ip, opts.interface)
+        socket.setMulticastTTL(opts.ttl || 255)
+        socket.setMulticastLoopback(opts.loopback !== false)
+      }
+    })
+
+    return socket
   }
 
-  binder(port)()
+  var receiveSocket = createSocket()
+  var sendSocket = createSocket()
 
-  that.send = function send (packet, cb) {
-    if (clientSocket) {
+  var sendBind = thunky(function (cb) {
+    sendSocket.on('error', cb)
+    sendSocket.bind(0, function () {
+      sendSocket.removeListener('error', cb)
+      cb(null, sendSocket)
+    })
+  })
+
+  receiveSocket.bind(port, function () {
+    that.emit('ready')
+  })
+
+  that.send = function (packet, cb) {
+    if (!cb) cb = noop
+    sendBind(function (err, socket) {
+      if (err) return cb(err)
       var message = packets.encode(packet)
-      clientSocket.send(message, 0, message.length, port, ip, cb)
-    } else {
-      binder(0)(function (err, socket) {
-        if (err) {
-          return cb(err)
-        }
-        clientSocket = socket
-        send(packet, cb)
-      })
-    }
-    // })
+      sendSocket.send(message, 0, message.length, port, ip, cb)
+    })
   }
 
   that.response =
@@ -105,10 +101,11 @@ module.exports = function (opts) {
 
   that.destroy = function (cb) {
     if (!cb) cb = noop
-    sockets.forEach(function (socket) {
-      socket.close()
+    sendSocket.once('close', function () {
+      receiveSocket.once('close', cb)
+      receiveSocket.close()
     })
-    cb()
+    sendSocket.close()
   }
 
   return that
