338acdbb6667c02eee9dbdcbe0ea835ae3f65b38
Thomas Watson Steen <w@tson.dk>
2016-01-05 17:30:39 +0100

    Clean up example.js
    Fix the following issues:
    - The .local domain was added twice
    - The response responded with an SRV-record to an A-record question

--- example.js
+++ example.js
@@ -1,5 +1,3 @@
-var local = require('os').hostname() + '.local'
-
 var mdns = require('./')()
 
 mdns.on('warning', function (err) {
@@ -12,32 +10,27 @@ mdns.on('response', function (response) {
 
 mdns.on('query', function (query) {
   console.log('got a query packet:', query)
-  // send a response for 'your-own-hostname.local'
-  if (query.questions.length && query.questions[0].name === local) {
-    mdns.respond({
-      answers: [{
-        name: 'my-service',
-        type: 'SRV',
-        data: {
-          port: 6666,
-          weigth: 0,
-          priority: 10,
-          target: local
-        }
-      }, {
-        name: local,
-        type: 'A',
-        ttl: 300,
-        data: '192.168.1.5'
-      }]
-    })
-  }
+
+  // iterate over all questions to check if we should respond
+  query.questions.forEach(function (q) {
+    if (q.type === 'A' && q.name === 'example.local') {
+      // send an A-record response for example.local
+      mdns.respond({
+        answers: [{
+          name: 'example.local',
+          type: 'A',
+          ttl: 300,
+          data: '192.168.1.5'
+        }]
+      })
+    }
+  })
 })
 
-// lets query for an A record for 'your-own-hostname.local'
+// lets query for an A-record for example.local
 mdns.query({
   questions: [{
-    name: local,
+    name: 'example.local',
     type: 'A'
   }]
 })
