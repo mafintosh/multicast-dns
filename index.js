bd19f2824b3eba02d014a438a037939bd5b2dcec
RangerMauve <RangerMauve@hotmail.com>
2018-08-13 14:54:50 -0400

    Get rid of extra spaces

--- index.js
+++ index.js
@@ -145,19 +145,19 @@ module.exports = function (opts) {
 }
 
 function defaultInterface () {
-   var networks = os.networkInterfaces()
-   var names = Object.keys(networks)
+  var networks = os.networkInterfaces()
+  var names = Object.keys(networks)
 
-   for (var i = 0; i < names.length; i++) {
-     var net = networks[names[i]]
-     for (var j = 0; j < net.length; j++) {
-       var iface = net[j]
-       if (iface.family === 'IPv4' && !iface.internal) return '0.0.0.0`
-     }
-   }
+  for (var i = 0; i < names.length; i++) {
+    var net = networks[names[i]]
+    for (var j = 0; j < net.length; j++) {
+      var iface = net[j]
+      if (iface.family === 'IPv4' && !iface.internal) return '0.0.0.0`
+    }
+  }
 
-   return '127.0.0.1'
- }
+  return '127.0.0.1'
+}
 
 function allInterfaces () {
   var networks = os.networkInterfaces()
